2f8176682c4c30a37e98286d304f7d2886a6422b
Kiyeon Jeon <kiyeonj21@gmail.com>
2021-02-26 14:23:15 +0900

    rest

--- dl/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import ConvNetModel1,ConvNetModel2
-from . import utils
-__all__ = [ConvNetModel1,ConvNetModel2,models, utils ]
--- experiments/tmp.py
+++
@@ -1,4 +0,0 @@
-from dl.utils import show_image
-show_image(dataset='fc_trainval')
-# show_image(dataset='tux')
-# show_image(dataset='action_trainval')
